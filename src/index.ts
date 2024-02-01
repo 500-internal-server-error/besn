66725c0fbc48594cd5d1e27e62f7013ca0f125f8
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2024-02-01 18:36:19 +0700

    Moved stuff into a `main` function

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 import { Client, IntentsBitField } from "discord.js";
 import * as jsonfile from "jsonfile";
 
-const config = jsonfile.readFileSync("./config.json");
+function main() {
+	const config = jsonfile.readFileSync("./config.json");
 
-const client = new Client({
-	intents: [
-		IntentsBitField.Flags.Guilds
-	]
-});
+	const client = new Client({
+		intents: [
+			IntentsBitField.Flags.Guilds
+		]
+	});
 
-client.on("ready", () => {
-	// Definitely not null since we are responding to the "ready" event, but ts doesn't know that
-	if (!client.user) return;
+	client.on("ready", () => {
+		// Definitely not null since we are responding to the "ready" event, but ts doesn't know that
+		if (!client.user) return;
 
-	console.log(`>>> Logged in as ${client.user.tag}`);
-	console.log(`>>> Bonjour!`);
-});
+		console.log(`>>> Logged in as ${client.user.tag}`);
+		console.log(`>>> Bonjour!`);
+	});
 
-client.login(config.token);
+	client.login(config.token);
+}
+
+main();
