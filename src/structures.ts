28253b6ac7e9bcf5fd71e83db7038b63244ee2e3
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2024-02-09 15:36:44 +0700

    Fixed virtual live scheduling using only the first schedule

--- src/structures.ts
+++ src/structures.ts
@@ -22,3 +22,20 @@ export interface ICommandHandler {
 	getSignature(): ChatInputApplicationCommandData;
 	handle(interaction: ChatInputCommandInteraction): Promise<void>;
 }
+
+// Not comprehensive, only includes details we are interested in
+
+export type VirtualLiveSchedule = {
+	virtualLiveId: number,
+	seq: number,
+	startAt: number,
+	endAt: number
+}
+
+export type VirtualLive = {
+	id: number,
+	name: string,
+	startAt: number,
+	endAt: number,
+	virtualLiveSchedules: VirtualLiveSchedule[]
+}
