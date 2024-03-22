0223ca1daeaf459c50ec1d58cfe9a53f34bc3e22
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2024-03-22 13:08:23 +0700

    Added formatting stuff
    ESLint + Prettier and their configs

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+export default {
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+	quoteProps: "preserve",
+	trailingComma: "none",
+	bracketSpacing: true,
+	arrowParens: "always",
+	requirePragma: true,
+	insertPragma: true,
+	proseWrap: "preserve",
+	endOfLine: "lf",
+	embeddedLanguageFormatting: "auto"
+}
